dd8c6588760eab1f25f4fff002c9f39fc2f228e9
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-06-21 18:03:33 +0200

    Fix missing globals and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,15 @@ import * as meta from "./package.json";
 
 const config = {
   input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
   output: {
     file: `dist/${meta.name}.js`,
     name: "d3-eplusm",
     format: "umd",
     indent: false,
     extend: true,
-    banner: `// ${meta.homepage} v${meta.version}`
+    banner: `// ${meta.homepage} v${meta.version}`,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
   },
   plugins: []
 };
